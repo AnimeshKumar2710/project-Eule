f978726a7d14d62bc70615f85548eabf9ea667cb
AnimeshKumar2710 <74147077+AnimeshKumar2710@users.noreply.github.com>
2021-01-22 20:06:55 +0530

    Update eular3.py

--- eular3.py
+++ eular3.py
@@ -1,7 +1,7 @@
 import sys
 
 def isprime(num):
-    for i in range (2, int(num / 2)):
+    for i in range (2, int(num / 2) + 1):
         if num % i == 0:
             return False
     return True
