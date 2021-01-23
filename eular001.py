e891bc0d1d5e796ce7cd12b5351401f3861c9620
AnimeshKumar2710 <74147077+AnimeshKumar2710@users.noreply.github.com>
2021-01-23 12:51:07 +0530

    Update and rename eular001.c to eular001.py

---
+++ eular001.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import sys
+
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    if n < 1:
+        print(0)
+    else:
+        a = int((n - 1) / 3)
+        b = int((n - 1) / 5)
+        c = int((n - 1) / 15)
+        a = 3 * int((a * (a + 1)) / 2)
+        b = 5 * int((b * (b + 1)) / 2)
+        c = 15 * int((c * (c + 1)) / 2)
+        print(a + b - c)
