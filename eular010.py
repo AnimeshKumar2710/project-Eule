6fa182665fc2d9942cec393e51ecff00acbdf15a
AnimeshKumar2710 <74147077+AnimeshKumar2710@users.noreply.github.com>
2021-01-23 19:41:04 +0530

    Create eular010.py

---
+++ eular010.py
@@ -0,0 +1,17 @@
+import sys
+
+def isprime(num):
+    for i in range (2, int(num / 2) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    sum = 0
+    for i in range (2, n + 1):
+        if isprime(i):
+            sum += i
+    
+    print(sum)
