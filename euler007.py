eb1f2c98ee07ec44710448df476788a0d68c7fb6
AnimeshKumar2710 <74147077+AnimeshKumar2710@users.noreply.github.com>
2021-01-28 12:52:24 +0530

    Update euler007.py

--- euler007.py
+++ euler007.py
@@ -1,20 +1,24 @@
+limit = 104801
 import sys
-
+import math
+prime = []
+prime.append(1)
+prime.append(2)
 def isprime(num):
-    for i in range (2, int(num / 2) + 1):
-        if num % i == 0:
-            return False
-    return True
+    check = 1
+    for i in range(1, len(prime)):
+        if num % prime[i] == 0:
+            check = 0
+            break
+        elif prime[i] > num //2:
+            break
+    if check == 1:
+        prime.append(num)
 
+for i in range (3,limit):
+    isprime(i)
+    
 t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
-    i = 2
-    count = 0
-    while 1:
-        if isprime(i):
-            count+= 1
-            if count == n:
-                print(i)
-                break
-        i += 1
+    print(prime[n])
